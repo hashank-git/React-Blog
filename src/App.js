cf0668c93542917b6ccbbb86d5119e55ec27cfc2
hashank-git <hashan.khan1@gmail.com>
2020-11-09 18:24:16 +0500

    Fetch Update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import Counter from './components/Counter'
-import Nav from './components/Nav'
-import Student from './components/Student'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  //const [name, setName] = useState("Hashan");
+  const [post, setPost] = useState({
+  
+  });
+
+  useEffect(() => {
+
+    axios.get("https://jsonplaceholder.typicode.com/posts/1")
+      .then(res => setPost(res.data))
+      .catch(err => console.log(err))
+
+  })
+
   return (
 
     <div className="App">
-      <Nav />
-      <h1> Hello World </h1>
-      <Student firstname='Muhammad'/>
-      <Student firstname='Hashan'/>
-      <Student firstname='Khan'/>
-      <Counter />
+
+      <h1>Fetch Data In React</h1>
+
+      <table>
+        <tr>
+          <td>Id</td>
+          <td> {post.id} </td>
+        </tr>
+        <tr>
+          <td>Title</td>
+          <td> {post.title} </td>
+        </tr>
+        <tr>
+          <td>Body</td>
+          <td> {post.body} </td>
+        </tr>
+      </table>
+
     </div>
   );
 }
---
+++ src/components/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+            <h3>Fetch Component</h3>
+        </div>
+    )
+}
+
+export default Post
