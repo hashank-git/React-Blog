08f71d0c6b4393695b43dc29ee08a55dc139f0c0
hashank-git <hashan.khan1@gmail.com>
2020-11-11 18:09:57 +0500

    Pages made

--- src/App.js
+++ src/App.js
@@ -1,44 +1,58 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-
+import {BrowserRouter, Route} from "react-router-dom";
+import Nav from './components/Nav'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Services from './Pages/Services'
 function App() {
 
-  //const [name, setName] = useState("Hashan");
-  const [post, setPost] = useState({
+                                  //const [name, setName] = useState("Hashan");
+                                  // const [post, setPost] = useState({});
+
+                                  // useEffect(() => {
+
+                                  //   axios.get("https://jsonplaceholder.typicode.com/posts/1")
+                                  //     .then(res => setPost(res.data))
+                                  //     .catch(err => console.log(err))
+
   
-  });
-
-  useEffect(() => {
-
-    axios.get("https://jsonplaceholder.typicode.com/posts/1")
-      .then(res => setPost(res.data))
-      .catch(err => console.log(err))
-
-  })
 
   return (
 
-    <div className="App">
+    
 
-      <h1>Fetch Data In React</h1>
+    <div>
+      <BrowserRouter>
+      <Nav />
+      <div className="container">
+      <Route exact path="/Home" component={Home} />
+      <Route path="/About" component={About} />
+      <Route path="/Contact" component={Contact} />
+      <Route path="/Services" component={Services} />
+      </div>
+      </BrowserRouter>
 
-      <table>
-        <tr>
-          <td>Id</td>
-          <td> {post.id} </td>
-        </tr>
-        <tr>
-          <td>Title</td>
-          <td> {post.title} </td>
-        </tr>
-        <tr>
-          <td>Body</td>
-          <td> {post.body} </td>
-        </tr>
-      </table>
+                                {/* <h1>Fetch Data In React</h1>
+
+                                <table>
+                                  <tr>
+                                    <td>Id</td>
+                                    <td> {post.id} </td>
+                                  </tr>
+                                  <tr>
+                                    <td>Title</td>
+                                    <td> {post.title} </td>
+                                  </tr>
+                                  <tr>
+                                    <td>Body</td>
+                                    <td> {post.body} </td>
+                                  </tr>
+                                </table> */}
 
     </div>
   );
-}
+                              }
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route, Link } from "react-router-dom";
 
 function Nav() {
     return (
@@ -12,25 +13,18 @@ function Nav() {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <Link className="nav-link" to="/Home">Home</Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
-      </li>
-      <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item" href="#">Action</a>
-          <a className="dropdown-item" href="#">Another action</a>
-          <div className="dropdown-divider"></div>
-          <a className="dropdown-item" href="#">Something else here</a>
-        </div>
+        <Link className="nav-link" to="About">About</Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link disabled" href="#">Disabled</a>
+        <Link className="nav-link" to="Contact">Contact</Link>
       </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="Services">Services</Link>
+      </li>
+     
     </ul>
     <form className="form-inline my-2 my-lg-0">
       <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
